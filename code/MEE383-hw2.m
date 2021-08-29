233044573c240856acbb19e72ee97c7c22478859
Kurt Jackson <kurtjackson@Kurts-MacBook-Pro.local>
2021-08-29 17:25:52 -0500

    added dirs and files

---
+++ code/MEE383-hw2.m
@@ -0,0 +1,12 @@
+zID = [1, 5, 3, 8, 10, 7, 8]
+
+A = zID(2)
+B = zID(3)
+C = zID(4)
+D = zID(5)
+E = zID(6)
+F = zID(7)
+
+A*(B+C)
+D / E - ((A*10)+F) % I had to use ((A*10)+F) instead of AF. AF registered as unknown function
+((B*10)+A) ^ C + (D - F) ^ E
