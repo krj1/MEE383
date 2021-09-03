9f5d18d22feee350fa57179308da08b699a78cd2
Kurt Jackson <kurtjackson@Kurts-MacBook-Pro.local>
2021-09-02 21:13:36 -0500

    made hw4 got basic requierments met

---
+++ code/MEE383_hw4.m
@@ -0,0 +1,33 @@
+zID = [1, 5, 3, 8, 10, 7, 8]
+
+A = zID(2)
+B = zID(3)
+C = zID(4)
+D = zID(5)
+E = zID(6)
+F = zID(7)
+
+xInc = 0:.05:100
+
+Gt = -F*exp(-abs((xInc/E)-C)).*cos(((2*pi)/E)*(xInc-(B/10)))-A
+GtPrime = diff(Gt)
+GtPrime2 = diff(GtPrime)
+
+i = 1
+yMax = 0
+
+
+while i < length(Gt)
+i = i+1
+    if Gt(i) > yMax
+        yMax = Gt(i)
+    end
+end
+iAmTooLazy = yMax * 0.44
+
+I = find(GtPrime2 >= 0 ) 
+
+plot(xInc,Gt,'b')
+hold on
+plot(xInc(I),Gt(I),'kd','Markersize',6,'MarkerFaceColor','k')
+hold off
