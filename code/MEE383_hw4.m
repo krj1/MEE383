53fffb271233e35dde11655a2f80b4cdc509adb2
Kurt Jackson <kurtjackson@Kurts-MBP.ISAAC>
2021-09-07 16:38:21 -0500

    fixed bugs, code works as I think the assignment wants. STABLE

--- code/MEE383_hw4.m
+++ code/MEE383_hw4.m
@@ -1,4 +1,4 @@
-zID = [1, 5, 3, 8, 10, 7, 8]
+zID = [1, 8, 7, 5, 1, 10, 1]
 
 A = zID(2)
 B = zID(3)
@@ -15,7 +15,7 @@ GtPrime = diff(Gt)
 GtPrime2 = diff(GtPrime)
 
 i = 1
-yMax = 0
+yMax = -A
 
 
 while i < length(Gt)
@@ -24,11 +24,16 @@ i = i+1
         yMax = Gt(i)
     end
 end
-iAmTooLazy = yMax * 0.44 - A
 
-I = find(GtPrime2 >= 0 ) 
+deltaY = yMax + A
+maxVal = deltaY * .44 - A
+minVal =  -A - deltaY * .44
+
+I = find(GtPrime2 >= 0 & Gt(1:end-2) > maxVal) 
+G = find(GtPrime2 >= 0 & Gt(1:end-2) < minVal)
 
 plot(xInc,Gt,'b')
 hold on
 plot(xInc(I),Gt(I),'kd','Markersize',6,'MarkerFaceColor','k')
+plot(xInc(G),Gt(G),'kd','Markersize',6,'MarkerFaceColor','k')
 hold off
