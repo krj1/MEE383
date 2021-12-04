ace75d7d00b6104e835db789257382ed58c28497
Kurt Jackson <kurtjackson@Kurts-MBP.ISAAC>
2021-12-04 11:34:41 -0600

    scrapping to save time

--- code/exam2/exam2_Q01.m
+++ code/exam2/exam2_Q01.m
@@ -21,15 +21,15 @@ unit_step = ((12+D)/12)*tand((16+F)/2);
 steps  = 3;
 A_small = zeros(steps*4-2,steps*4-2);
 A_large = zeros((12-steps*2)*4, (12-steps*2)*4);
-A = zeros(45,45)
+A = zeros(45,45);
 
 
 
 A_small(1,1:2)= [slant_uv(1),slant_uv(1)]; %x A
 A_small(2,1:2)= [slant_uv(2),-slant_uv(2)]; %y A
 
-A_small(3,1:5)= [-slant_uv(1),0,0,0,slant_uv(1),]; %x B
-A_small(4,1:5)= [-slant_uv(2),0,-1,0,-slant_uv(2),]; %y B
+A_small(3,1:4)= [-slant_uv(1),0,0,slant_uv(1),]; %x B
+A_small(4,1:4)= [-slant_uv(2),0,-1,slant_uv(2),]; %y B
 
 p = [];
 k = [];
@@ -50,13 +50,15 @@ for i = 1:steps
         A_small(10,6:10) = [slant_uv(2),1,0,0,-slant_uv(2)];
         
     elseif i > 1 && mod(i,2) == 0
-        unit_delta_bar = [((12+D)/12)/(sqrt(((12+D)/12)+((12+D)/12)*(2*i-1)*tand((16+F)/2))),(((12+D)/12)*(2*i-1)*tand((16+F)/2))/(sqrt(((12+D)/12)+((12+D)/12)*(2*i-1)*tand((16+F)/2)))];
+        unit_delta_bar_left = [-1*((12+D)/12)/(sqrt(((12+D)/12)+((12+D)/12)*(2*i-3)*tand((16+F)/2))),-1*(((12+D)/12)*(2*i-3)*tand((16+F)/2))/(sqrt(((12+D)/12)+((12+D)/12)*(2*i-3)*tand((16+F)/2)))];
+        
+        unit_delta_bar_right = [((12+D)/12)/(sqrt(((12+D)/12)+((12+D)/12)*(2*i-1)*tand((16+F)/2))),-1*(((12+D)/12)*(2*i-1)*tand((16+F)/2))/(sqrt(((12+D)/12)+((12+D)/12)*(2*i-1)*tand((16+F)/2)))];
         
         A_small(3,1:5)= [-slant_uv(1),0,0,0,slant_uv(1),]; %x B
         A_small(4,1:5)= [-slant_uv(2),0,-1,0,-slant_uv(2),]; %y B
         
-        A_small(5,1:6)= [0,-slant_uv(1) 0, unit_delta_bar(1), 0, slant_uv(1)]; %x C
-        A_small(6,1:6)= [0,slant_uv(2), 1, unit_delta_bar(2), 0, -slant_uv(2)]; %y C
+        A_small(5,1:6)= [0,-slant_uv(1) 0, unit_delta_bar_right(1), 0, slant_uv(1)]; %x C
+        A_small(6,1:6)= [0,slant_uv(2), 1, unit_delta_bar_right(2), 0, -slant_uv(2)]; %y C
         
         
         
@@ -68,6 +70,33 @@ end
 
 
 
+for i = 1:1:5
+    if i > 1 && mod(i,2) == 1
+        unit_delta_bar_left = [-1*((12+D)/12)/(sqrt(((12+D)/12)+((12+D)/12)*(2*steps-3)*tand((16+F)/2))),-1*(((12+D)/12)*(2*steps-3)*tand((16+F)/2))/(sqrt(((12+D)/12)+((12+D)/12)*(2*steps-3)*tand((16+F)/2)))];
+        
+        unit_delta_bar_right = [((12+D)/12)/(sqrt(((12+D)/12)+((12+D)/12)*(2*steps-1)*tand((16+F)/2))),-1*(((12+D)/12)*(2*steps-1)*tand((16+F)/2))/(sqrt(((12+D)/12)+((12+D)/12)*(2*steps-1)*tand((16+F)/2)))];
+        
+        A_large(9,7:12) = [unit_delta_bar_left(1),-1,0,0,unit_delta_bar_right(1),1]; %x
+        A_large(10,7:12) = [-unit_delta_bar_left(2),0,1,0,-unit_delta_bar_right(2),0]; %y
+        
+        A_large(11,6:10) = [-1,0,0,0,1]; %x
+        A_large(12,6:10) = [0,0,0,-1,0]; %y
+        -i
+        
+    elseif i > 1 && mod(i,2) == 0
+        unit_delta_bar_left = [-1*((12+D)/12)/(sqrt(((12+D)/12)+((12+D)/12)*(2*steps-3)*tand((16+F)/2))),-1*(((12+D)/12)*(2*steps-3)*tand((16+F)/2))/(sqrt(((12+D)/12)+((12+D)/12)*(2*steps-3)*tand((16+F)/2)))];
+        i
+        unit_delta_bar_right = [((12+D)/12)/(sqrt(((12+D)/12)+((12+D)/12)*(2*steps-1)*tand((16+F)/2))),-1*(((12+D)/12)*(2*steps-1)*tand((16+F)/2))/(sqrt(((12+D)/12)+((12+D)/12)*(2*steps-1)*tand((16+F)/2)))];
+          
+        A_large(5,2:7) = [-1,unit_delta_bar_left(1),0,0,1,unit_delta_bar_right(1)]; %x
+        A_large(6,2:7) = [0,unit_delta_bar_left(2),0,-1,0,unit_delta_bar_right(2)]; %y
+        
+        A_large(7,4:8) = [-1,0,0,0,1]; %x
+        A_large(8,4:8) = [0,1,0,0,0]; %y
+        
+    end
+    
+end
 
 
 
